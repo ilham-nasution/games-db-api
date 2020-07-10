3fe5d2f3d0d942821417f323291352b859605679
Ilham Rahman <ilhaamrahman@gmail.com>
2020-07-10 11:05:56 +0700

    add some seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+puts 'Creating Developers...'
+developers = ['Square Enix', 'CD Projekt Red', 'Capcom', 'Konami', 'Rockstar Games', 'Electronic Arts', 'Ubisoft']
+developers.each do |developer|
+  Developer.create!(name: developer)
+end
+puts '...Finished'
+
+puts 'Creating Platforms...'
+platforms = ['Playstation 4', 'Nintendo Switch', 'Xbox One']
+platforms.each do |platform|
+  Developer.create!(name: platform)
+end
+puts '...finished'
